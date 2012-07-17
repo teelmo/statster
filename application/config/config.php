755ce0dd318bd22d33d825dd4ae8ac4335b6168c
Teemo Tebest <teemo.tebest@gmail.com>
2012-07-17 14:33:41 +0300

    Really now added the session library to autoload

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '7149c57ae837dc36b910fe79a5558ddd';
 
 /*
 |--------------------------------------------------------------------------
