9eddc26f253a6ef8c0bd23f3eb469622a3349370
Teemo Tebest <teemo.tebest@gmail.com>
2012-07-19 21:08:31 +0300

    Implemented the footer links which point to meta pages and include all rights reserved.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,14 @@
       </div>
     </div>
     <div id="footer">
+      <p>
+        <?=anchor(array('about'), 'About', array('title' => 'About Statster'))?>&nbsp; | &nbsp;
+        <?=anchor(array('terms'), 'Terms', array('title' => 'Terms of service'))?>&nbsp; | &nbsp;
+        <?=anchor(array('privacy'), 'Privacy', array('title' => 'Privacy policy'))?>&nbsp; | &nbsp;
+        <?=anchor(array('career'), 'Career', array('title' => 'Career'))?>&nbsp; | &nbsp;
+        <?=anchor(array('developers'), 'Developers', array('title' => 'Developers'))?>&nbsp; | &nbsp;
+        &copy; Statster 2007 <?=DASH?> 2012. All rights reserved.
+      </p>
       <script type="text/javascript">
         <?php
         if(isset($request)) {
