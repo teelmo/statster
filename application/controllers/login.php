c0438aed25d68933b31e44f18a4b5fcdda64c3a1
Teemo Tebest <teemo.tebest@gmail.com>
2012-07-24 10:21:50 +0300

    Added the deliver of the request parameter to the login controller

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -4,9 +4,13 @@ class Login extends CI_Controller {
   public function index() {
     // Load helpers
     $this->load->helper(array('form'));
+
+    $data = array();
+    $data['request'] = 'login';
+
     $this->load->view('templates/header');
     $this->load->view('login_view');
-    $this->load->view('templates/footer');  
+    $this->load->view('templates/footer', $data);  
   }
 }
 ?>
