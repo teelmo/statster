5096ccdb36352dc56ea3fc1449511bc42d75b598
Teemo Tebest <teemo.tebest@gmail.com>
2012-07-27 14:58:41 +0300

    Implemented the top bar further. Added some icons and divider elements.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -29,28 +29,36 @@
       <div id="topContInner">
         <h1><?=anchor('/', 'Statster')?></h1>
         <img src="/media/img/beta.png" alt="" id="betaLogo" />
-        <div class="floatRight" id="userCont">
-          <?php
-          if($this->session->userdata('logged_in') === TRUE) {
+        <div class="floatRight">
+          <div id="searchCont">
+            <form action="http://beta.statster.info/" method="post" accept-charset="utf-8" class="" id="searchForm">
+              <div style="display: none;">
+                <input type="hidden" name="searchType" value="form">
+              </div>
+              <input type="text" class="middle" autocomplete="off" tabindex="10" id="searchString" placeholder="search.." name="searchStr" />
+              <button id="searchSubmit" type="submit" class="submit" title="Search"></button>
+            </form>
+          </div>
+          <span class="divider"></span>
+          <div id="userCont">
+            <?php
+            if($this->session->userdata('logged_in') === TRUE) {
+              ?>
+              <!--<?=anchor('logout', 'Logout')?>-->
+              <?=anchor('user/'.$this->session->userdata('username'), $this->session->userdata('real_name') . ' <img src="' . $this->session->userdata('user_image') . '" alt="" class="userImg userImg32" id="profile"/>')?>
+              <?
+            }
+            else {
+              ?>
+              <?=anchor(array('login'), 'Login', array('title' => 'Login'))?>
+              <?
+            }
             ?>
-            <!--<?=anchor('logout', 'Logout')?>-->
-            <?=anchor('user/'.$this->session->userdata('username'), '<img src="' . $this->session->userdata('user_image') . '" alt="" class="userImg userImg32" style="vertical-align: -45%;"/>')?>
-            <?
-          }
-          else {
-            ?>
-            <?=anchor(array('login'), 'Login', array('title' => 'Login'))?>
-            <?
-          }
-          ?>
-        </div>
-        <div class="floatRight" id="searchCont">
-          <form action="http://beta.statster.info/" method="post" accept-charset="utf-8" class="" id="searchForm">
-            <div style="display: none;">
-              <input type="hidden" name="searchType" value="form">
-            </div>
-            <input type="text" class="middle" autocomplete="off" tabindex="10" id="searchString" placeholder="Search.." name="searchStr" />
-          </form>
+          </div>
+          <span class="divider"></span>
+          <div>
+            <?=anchor('/', '<img src="/media/img/icons/bar_chart_28px.png" alt="" id="addlistening"/>')?>
+          </div>
         </div>
       </div>
     </div>
