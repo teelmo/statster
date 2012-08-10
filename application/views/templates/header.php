30fb71e38d15891b2ebd6f2c871ca8be0bdd035f
Teemo Tebest <teemo.tebest@gmail.com>
2012-08-10 14:31:21 +0300

    Small changes to the header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -28,33 +28,35 @@
     <div id="topCont">
       <div id="topContInner">
         <h1><?=anchor('/', 'Statster')?></h1>
-        <img src="/media/img/beta.png" alt="" id="betaLogo" />
+        <img src="/media/img/icons/beta.png" alt="" id="betaLogo" />
         <div class="floatRight">
           <div id="searchCont">
             <form action="http://beta.statster.info/" method="post" accept-charset="utf-8" class="" id="searchForm">
               <div style="display: none;">
                 <input type="hidden" name="searchType" value="form">
               </div>
-              <input type="text" class="middle" autocomplete="off" tabindex="10" id="searchString" placeholder="search.." name="searchStr" />
-              <button id="searchSubmit" type="submit" class="submit" title="Search"></button>
+              <input type="text" class="middle searchForm" autocomplete="off" tabindex="10" id="searchString" placeholder="search.." name="searchStr" />
+              <button id="searchSubmit" type="submit" class="submit searchForm" title="Search"></button>
             </form>
           </div>
           <span class="divider"></span>
-          <div id="userCont">
             <?php
             if($this->session->userdata('logged_in') === TRUE) {
               ?>
-              <!--<?=anchor('logout', 'Logout')?>-->
-              <?=anchor('user/'.$this->session->userdata('username'), $this->session->userdata('real_name') . '&nbsp;&nbsp;<img src="' . $this->session->userdata('user_image') . '" alt="" class="userImg userImg32" id="profile"/>')?>
-              <span class="divider"></span>
+              <div id="userCont">
+                <?=anchor('user/'.$this->session->userdata('username'), $this->session->userdata('real_name') . '&nbsp;&nbsp;<img src="' . $this->session->userdata('user_image') . '" alt="" class="userImg userImg20" id="profile"/>')?>
+                <span class="divider"></span>
+              </div>
               <div>
-                <?=anchor('/', '<img src="/media/img/icons/bar_chart_28px.png" alt="" id="addlistening"/>')?>
+                <?=anchor('/', '<img src="/media/img/icons/bar_chart_20px.png" alt="" id="addlistening"/>')?>
               </div>
               <?
             }
             else {
               ?>
-              <?=anchor(array('login'), 'Login', array('title' => 'Login'))?>
+              <div id="userCont">
+                <?=anchor(array('login'), 'Login', array('title' => 'Login'))?>
+              </div>
               <?
             }
             ?>
