40f106a9a11c19c27b5cea25e8cd4d914f6c1329
Teemo Tebest <teemo.tebest@gmail.com>
2012-09-13 20:42:17 +0300

    Small progress with the inbox_view.

--- application/views/inbox/inbox_view.php
+++ application/views/inbox/inbox_view.php
@@ -1,6 +1,19 @@
 <div id="leftCont">
   <div class="container">
-    <h1>Inbox</h1>
+    <h1>Compose</h1>
+    <?=form_open('', array('class' => '', 'id' => 'composeForm'), array('composeType' => 'form'))?>
+      <div>
+        <input type="text" autocomplete="off" tabindex="1" id="composeRecipent" placeholder="Enter recipent(s)" class="composeText" name="composeRecipent" />
+      </div>
+      <div>
+        <textarea tabindex="2"></textarea>
+      </div>
+    </form>
+  </div>
+  <div class="container"><hr /></div>
+  <div class="container">
+    <h1>Your inbox</h1>
+    
   </div>
   <div class="container"><hr /></div>
 </div>
