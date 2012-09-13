73c5e90ac094d6dbb5468ffb9fe890d92e09c0b2
Teemo Tebest <teemo.tebest@gmail.com>
2012-09-13 20:37:17 +0300

    Started to implement a function that would return all users in the system.

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -74,8 +74,45 @@ if (!function_exists('logoutUser')) {
   function logoutUser($opts = array()) {
     $ci=& get_instance();
 
-    $userdata = array('logged_in'    => FALSE);
+    $userdata = array('logged_in' => FALSE);
     $ci->session->set_userdata($userdata);
   }
 }
+/**
+   * Handles user login form posts.
+   *
+   * @param array $opts.
+   *          'artist'  => Artist name
+   *
+   * @return int artist ID or boolean FALSE.
+   *
+   * @todo Destroy session. 
+   */
+if (!function_exists('getUsers')) {
+  function getUsers($opts = array()) {
+    $ci=& get_instance();
+    $ci->load->database();
+
+    $exclude_username = explode(',', $opts['exclude_username']);
+    $sql = "SELECT " . TBL_user . ".`id`,
+                   " . TBL_user . ".`username`,
+                   " . TBL_user_info . ".`email`,
+                   " . TBL_user_info . ".`real_name`,
+                   " . TBL_user_info . ".`lastfm_name`,
+                   " . TBL_user_info . ".`gender`,
+                   " . TBL_user . ".`created`,
+                   " . TBL_user . ".`last_login`,
+                   " . TBL_user . ".`last_access`
+            FROM " . TBL_user . ",
+                 " . TBL_user_info . "
+            WHERE " . TBL_user . ".`id` = " . TBL_user_info . ".`user_id`
+              AND " . TBL_username. ".`username` NOT IN (" . $exclude_username . ")";
+    $query = $ci->db->query($sql);
+    if ($query->num_rows() == 1) {
+      $ci->load->helper(array('img_helper'));
+      $result = $query->result();
+      $result = $result[0];
+    }
+  }
+}  
 ?>
