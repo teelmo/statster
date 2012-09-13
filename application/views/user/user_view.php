90bda6e21d510c9b65cf1ccbc911488c6810a5b8
Teemo Tebest <teemo.tebest@gmail.com>
2012-09-13 20:44:22 +0300

    Beginning to implement user_view

--- application/views/user/user_view.php
+++ application/views/user/user_view.php
@@ -1,12 +1,17 @@
 <div id="leftCont">
   <div class="container">
     <h1>Users</h1>
+    Mosaik of users
   </div>
   <div class="container"><hr /></div>
 </div>
 
 <div id="rightCont">
   <div class="container">
-    <h1></h1>
+    <h1>Statistics</h1>
+    Total number of users
+    <h2>Newest user</h2>
+
+    <h2></h2>
   </div>
 </div>
