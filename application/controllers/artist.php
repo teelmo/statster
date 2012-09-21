8a72337f93fcf011381a4f3fa660a9ec854b13a3
Teemo Tebest <teemo.tebest@gmail.com>
2012-09-21 21:00:45 +0300

    Made preparations for artists page.

--- application/controllers/artist.php
+++ application/controllers/artist.php
@@ -2,9 +2,11 @@
 class Artist extends CI_Controller {
 
   public function index() {
+    $data['request'] = 'artists';
+
     $this->load->view('templates/header');
     $this->load->view('artists_view');
-    $this->load->view('templates/footer');
+    $this->load->view('templates/footer', $data);
   }
 
 }
--- application/views/artists_view.php
+++ application/views/artists_view.php
@@ -2,11 +2,26 @@
   <div class="container">
     <h1>Artists</h1>
   </div>
+  <div class="container">
+    <img src="/media/img/ajax-loader-bar.gif" alt="" class="loader" id="allArtistsLoader"/>
+    <table id="allArtists"><!-- Content is loaded with AJAX --></table>
+  </div>
   <div class="container"><hr /></div>
 </div>
 
 <div id="rightCont">
   <div class="container">
-    
+    <h1>Yearly top artists</h1>
+
+    <h2>2012</h2>
+    <h2>2011</h2>
+    <h2>2010</h2>
+    <h2>2009</h2>
+    <h2>2008</h2>
+    <h2>2007</h2>
+    <h2>2006</h2>
+    <h2>2005</h2>
+    <h2>2004</h2>
+    <h2>2003</h2>
   </div>
 </div>
