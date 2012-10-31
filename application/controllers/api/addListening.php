baf3db19b57401000e096c37aec1fc776e819a97
Teemo Tebest <teemo.tebest@gmail.com>
2012-10-31 18:32:04 +0200

    Fixed the bug with add listening application which caused that all listenings where added to user_id 1

--- application/controllers/api/addListening.php
+++ application/controllers/api/addListening.php
@@ -9,7 +9,6 @@ class AddListening extends CI_Controller {
     }
     if (strpos($_POST['text'], DASH)) {
       $data = array();
-      $data['username'] = 'teelmo';
       list($data['artist'], $data['album']) = explode(DASH, $_POST['text']);
       $data['artist'] = trim($data['artist']);
       $data['album'] = preg_replace('/^(.*)\(([0-9]){4}\)$/', '$1', $data['album']);
@@ -20,7 +19,7 @@ class AddListening extends CI_Controller {
         echo json_encode(array('error' => array('msg' => 'Album error. Can\'t solve album id.')));
         return FALSE;
       }
-      if (!$data['user_id'] = getUserID($data)) {
+      if (!$data['user_id'] = $this->session->userdata('user_id')) {
         echo json_encode(array('error' => array('msg' => 'Username error. Can\'t solve user id.')));
         return FALSE;
       }
