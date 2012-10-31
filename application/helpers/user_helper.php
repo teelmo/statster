ecadc32fe594aa0c7bff0cd85448820b0d1437df
Teemo Tebest <teemo.tebest@gmail.com>
2012-10-31 22:01:44 +0200

    Changed inlining.

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -47,9 +47,7 @@ if (!function_exists('loginUser')) {
                    'created'      => $result->created,
                    'last_login'   => $result->last_login,
                    'last_access'  => $result->last_access,
-                   'user_image'   => getUserImg(array('user_id' => $result->id, 
-                                                        'size'    => 32
-                                                        )),
+                   'user_image'   => getUserImg(array('user_id' => $result->id, 'size'    => 32)),
                    'logged_in'    => TRUE
                );
       $ci->session->set_userdata($userdata);
