833a0871889f0302291cb44c14613a8e91f53d22
Teemo Tebest <teemo.tebest@gmail.com>
2012-11-04 16:27:49 +0200

    Added a divider between biography and similar artists on album page.

--- application/views/album_view.php
+++ application/views/album_view.php
@@ -66,6 +66,7 @@
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu. Sed arcu lectus auctor vitae, consectetuer et venenatis eget velit. Sed augue orci, lacinia eu tincidunt et eleifend nec lacus. Donec ultricies nisl ut felis, suspendisse potenti. Lorem ipsum ligula ut hendrerit mollis, ipsum erat vehicula risus, eu suscipit sem libero nec erat. Aliquam erat volutpat. Sed congue augue vitae neque. Nulla consectetuer porttitor pede. Fusce purus morbi tortor magna condimentum vel, placerat id blandit sit amet tortor.
     </p>
   </div>
+  <div class="container"><hr /></div>
   <div class="container">
     <h2>Similar artists</h2>
     <img src="/media/img/ajax-loader-bar.gif" alt="" class="loader" id="similarArtistLoader"/>
