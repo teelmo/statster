adb985d0a808f4335dbe1bcd926c0f36a5a3af83
Teemo Tebest <teemo.tebest@gmail.com>
2012-11-07 22:13:56 +0200

    Added a new constant for ERROR no events.

--- application/config/constants.php
+++ application/config/constants.php
@@ -51,6 +51,7 @@ define('DASH', '-');
 // Error msgs
 define('ERR_NO_RESULTS', 'No results.');
 define('ERR_NO_ARTIST', 'No artist given.');
+define('ERR_NO_EVENT', 'No events.');
 define('ERR_GENERAL', 'General error.');
 define('ERR_INCORRECT_CREDENTIALS', 'Username or password error.');
 
