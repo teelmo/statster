ecbd059ef00e5e68c4ff8fe55c35d1d863f4b805
Teemo Tebest <teemo.tebest@gmail.com>
2012-11-11 00:38:02 +0200

    Reorganized listening api a little bit.

--- application/controllers/api/listening.php
+++ application/controllers/api/listening.php
@@ -21,32 +21,39 @@ class Listening extends CI_Controller {
     }
     if (strpos($_POST['text'], DASH)) {
       $data = array();
-      list($data['artist'], $data['album']) = explode(DASH, $_POST['text']);
-      $data['artist'] = trim($data['artist']);
-      $data['album'] = preg_replace('/^(.*)\(([0-9]){4}\)$/', '$1', $data['album']);
-      $data['album'] = trim($data['album']);
-      $data['date'] = trim($_POST['date']);
-      if (!$data['album_id'] = getAlbumID($data)) {
-        echo json_encode(array('error' => array('msg' => 'Album error. Can\'t solve album id.')));
-        return FALSE;
-      }
+      
+      // Get user id from session
       if (!$data['user_id'] = $this->session->userdata('user_id')) {
         echo json_encode(array('error' => array('msg' => 'Username error. Can\'t solve user id.')));
         return FALSE;
       }
+      list($data['artist'], $data['album']) = explode(DASH, $_POST['text']);
+      $data['artist'] = trim($data['artist']);
+      $data['album'] = preg_replace('/^(.*)\(([0-9]){4}\)$/', '$1', $data['album']);
+      $data['album'] = trim($data['album']);
+      // Check that album exists
+      if (!$data['album_id'] = getAlbumID($data)) {
+        echo json_encode(array('error' => array('msg' => 'Album error. Can\'t solve album id.')));
+        return FALSE;
+      }
+      $data['date'] = trim($_POST['date']);
 
-      // Add data to DB
+      // Add listening data to DB
       $sql = "INSERT
                 INTO " . TBL_listening . " (`user_id`, `album_id`, `date`)
                 VALUES ({$data['user_id']}, {$data['album_id']}, '{$data['date']}')";
       $query = $this->db->query($sql);
       if ($this->db->affected_rows() == 1) {
         $data['listening_id'] = $this->db->insert_id();
-        // Add listening format data
+        // Add listening format data to DB
         if (!empty($_POST['format'])) {
           list($data['format'], $data['format_type']) = explode(':', $_POST['format']);
           addListeningFormat($data);
         }
+      }
+      else {
+        echo json_encode(array('error' => array('msg' => ERR_GENERAL)));
+        return FALSE;
       }
     }
     else {
