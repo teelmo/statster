aad44431d283755fb1cba580fe96f7da5481d0b5
Teemo Tebest <teemo.tebest@gmail.com>
2012-11-11 16:02:20 +0200

    Moved add listening and add format functionalities from listening controller and music_helper to a new helper called listening_helper.

--- application/controllers/api/listening.php
+++ application/controllers/api/listening.php
@@ -6,60 +6,16 @@ class Listening extends CI_Controller {
 
   public function get() {
     // Load helpers
-    $this->load->helper(array('music_helper', 'return_helper'));
+    $this->load->helper(array('listening_helper', 'return_helper', 'love_helper', 'output_helper'));
     
-    echo getRecentlyListened($_REQUEST);
+    echo getListenings($_REQUEST);
   }
 
   public function add() {
     // Load helpers
-    $this->load->helper(array('id_helper', 'music_helper'));
+    $this->load->helper(array('listening_helper', 'id_helper'));
 
-    if (empty($_POST)) {
-      echo json_encode(array('error' => array('msg' => '$_POST parameters not delivered')));
-      return FALSE;
-    }
-    if (strpos($_POST['text'], DASH)) {
-      $data = array();
-      
-      // Get user id from session
-      if (!$data['user_id'] = $this->session->userdata('user_id')) {
-        echo json_encode(array('error' => array('msg' => 'Username error. Can\'t solve user id.')));
-        return FALSE;
-      }
-      list($data['artist'], $data['album']) = explode(DASH, $_POST['text']);
-      $data['artist'] = trim($data['artist']);
-      $data['album'] = preg_replace('/^(.*)\(([0-9]){4}\)$/', '$1', $data['album']);
-      $data['album'] = trim($data['album']);
-      // Check that album exists
-      if (!$data['album_id'] = getAlbumID($data)) {
-        echo json_encode(array('error' => array('msg' => 'Album error. Can\'t solve album id.')));
-        return FALSE;
-      }
-      $data['date'] = trim($_POST['date']);
-
-      // Add listening data to DB
-      $sql = "INSERT
-                INTO " . TBL_listening . " (`user_id`, `album_id`, `date`)
-                VALUES ({$data['user_id']}, {$data['album_id']}, '{$data['date']}')";
-      $query = $this->db->query($sql);
-      if ($this->db->affected_rows() == 1) {
-        $data['listening_id'] = $this->db->insert_id();
-        // Add listening format data to DB
-        if (!empty($_POST['format'])) {
-          list($data['format'], $data['format_type']) = explode(':', $_POST['format']);
-          addListeningFormat($data);
-        }
-      }
-      else {
-        echo json_encode(array('error' => array('msg' => ERR_GENERAL)));
-        return FALSE;
-      }
-    }
-    else {
-      echo json_encode(array('error' => array('msg' => 'Format error.')));
-      return FALSE;
-    }
+    echo addListening($_POST);
   }
 
   /* Update listening information */
---
+++ application/helpers/listening_helper.php
@@ -0,0 +1,203 @@
+<?php 
+if (!defined('BASEPATH')) exit ('No direct script access allowed');
+
+/**
+  * Returns recently listened albums for the given user.
+  *
+  * @param array $opts.
+  *          'username'        => Username
+  *          'artist_name'          => Artist name
+  *          'album_name'           => Album name
+  *          'date'            => Listening date in yyyy/mm/dd format
+  *          'limit'           => Limit
+  *          'human_readable'  => Output format
+  *
+  * @return string JSON.
+  */
+if (!function_exists('getListenings')) {
+  function getListenings($opts = array()) {
+    $ci=& get_instance();
+    $ci->load->database();
+    
+    $username = !empty($opts['username']) ? $opts['username'] : '%';
+    $artist_name = !empty($opts['artist_name']) ? $opts['artist_name'] : '%';
+    $album_name = !empty($opts['album_name']) ? $opts['album_name'] : '%';
+    $date = !empty($opts['date']) ? $opts['date'] : '%';
+    $limit = !empty($opts['limit']) ? $opts['limit'] : 10;
+    $human_readable = !empty($opts['human_readable']) ? $opts['human_readable'] : FALSE;
+    $sql = "SELECT " . TBL_listening . ". `id` as `listening_id`,
+                   " . TBL_artist . ". `artist_name`,
+                   " . TBL_album . ". `album_name`,
+                   " . TBL_album . ". `year`, 
+                   " . TBL_user . ". `username`,
+                   " . TBL_listening . ". `date`, 
+                   " . TBL_listening . ". `created`,
+                   " . TBL_artist . ". `id` as `artist_id`,
+                   " . TBL_album . ". `id` as `album_id`,
+                   " . TBL_user . ". `id` as `user_id`
+            FROM " . TBL_album . ", " . TBL_artist . ", " . TBL_listening . ", " . TBL_user . "
+            WHERE " . TBL_album . ". `id` = " . TBL_listening . ". `album_id`
+              AND " . TBL_user . ". `id` = " . TBL_listening . ". `user_id`
+              AND " . TBL_artist . ". `id` = " . TBL_album . ". `artist_id`
+              AND " . TBL_user . ". `username` LIKE " . $ci->db->escape($username) . "
+              AND " . TBL_artist . ". `artist_name` LIKE " . $ci->db->escape($artist_name) . "
+              AND " . TBL_album . ". `album_name` LIKE " . $ci->db->escape($album_name) . "
+              AND " . TBL_listening . ". `date` LIKE " . $ci->db->escape($date) . "
+            ORDER BY " . TBL_listening . ". `date` DESC, 
+                     " . TBL_listening . ". `id` DESC
+            LIMIT " . mysql_real_escape_string($limit);
+    $query = $ci->db->query($sql);
+    return _json_return_helper($query, $human_readable);
+  }
+}
+
+/**
+  * Add listening data
+  *
+  * @param array $opts.
+  *
+  * @return string JSON.
+  */
+if (!function_exists('addListening')) {
+  function addListening($opts = array()) {
+    if (empty($opts)) {
+      header("HTTP/1.1 400 Bad Request");
+      return json_encode(array('error' => array('msg' => '$_POST parameters not delivered')));
+    }
+    if (strpos($opts['text'], DASH)) {
+      $ci=& get_instance();
+      $ci->load->database();
+
+      $data = array();
+      
+      // Get user id from session
+      if (!$data['user_id'] = $ci->session->userdata('user_id')) {
+        header("HTTP/1.1 401 Unauthorized");
+        return json_encode(array('error' => array('msg' => $data)));
+      }
+      list($data['artist'], $data['album']) = explode(DASH, $opts['text']);
+      $data['artist'] = trim($data['artist']);
+      $data['album'] = preg_replace('/^(.*)\(([0-9]){4}\)$/', '$1', $data['album']);
+      $data['album'] = trim($data['album']);
+      // Check that album exists
+      if (!$data['album_id'] = getAlbumID($data)) {
+        header("HTTP/1.1 404 Not Found");
+        return json_encode(array('error' => array('msg' => $data)));
+      }
+      $data['date'] = trim($opts['date']);
+
+      // Add listening data to DB
+      $sql = "INSERT
+                INTO " . TBL_listening . " (`user_id`, `album_id`, `date`)
+                VALUES ({$data['user_id']}, {$data['album_id']}, '{$data['date']}')";
+                echo $sql;
+      $query = $ci->db->query($sql);
+      if ($ci->db->affected_rows() == 1) {
+        $data['listening_id'] = $ci->db->insert_id();
+        // Add listening format data to DB
+        if (!empty($_POST['format'])) {
+          list($data['format'], $data['format_type']) = explode(':', $_POST['format']);
+          addListeningFormat($data);
+        }
+        header("HTTP/1.1 201 Created");
+        return json_encode(array('success' => array('msg' => $data)));
+      }
+      else {
+        header("HTTP/1.1 400 Bad Request");
+        return json_encode(array('error' => array('msg' => ERR_GENERAL)));
+      }
+    }
+    else {
+      header("HTTP/1.1 404 Not Found");
+      return json_encode(array('error' => array('msg' => 'Format error.')));
+    }
+  }
+}
+
+/**
+  * Collection function for adding format and
+  * format type information to a listening.
+  *
+  * @param array $opts.
+  *          'format'      => Format name
+  *          'format_type' => Format type name
+  *
+  * @return boolean TRUE.
+  */
+if (!function_exists('addListeningFormat')) {
+  function addListeningFormat($data = array()) {
+    $ci=& get_instance();
+    $ci->load->database();
+
+    $data['format_id'] = getFormatID($data);
+    $data['format_type_id'] = getFormatTypeID($data);
+    (!empty($data['format_id'])) ? addListeningFormats($data) : FALSE;
+    (!empty($data['format_type_id'])) ?  addListeningFormatTypes($data) : FALSE;
+
+    return TRUE;
+  }
+}
+
+/**
+ * Attaches the given format to the given listening.
+ *
+ * @param array $opts.
+ *          'format_id'     => Format ID
+ *          'listening_id'  => Listening ID
+ *          'user_id'       => User ID
+ *
+ * @return int Insert ID or boolean FALSE.
+ */
+if (!function_exists('addListeningFormats')) {
+  function addListeningFormats($opts = array()) {
+    $ci=& get_instance();
+    $ci->load->database();
+
+    $format_id = !empty($opts['format_id']) ? $opts['format_id'] : '';
+    $listening_id = $opts['listening_id'];
+    $user_id = $opts['user_id'];
+    
+    $sql = "INSERT
+              INTO " . TBL_listening_formats . " (`listening_id`, `listening_format_id`, `user_id`) 
+              VALUES (" . $ci->db->escape($listening_id) . ", " . $ci->db->escape($format_id) . ", " . $ci->db->escape($user_id) . ")";
+    $query = $ci->db->query($sql);
+    if($ci->db->affected_rows() == 1) {
+      return $ci->db->insert_id();
+    }
+    else {
+      return FALSE;
+    }
+  }
+}
+
+/**
+  * Attaches the given format type to the given listening.
+  *
+  * @param array $opts.
+  *          'format_type_id'  => Format type ID
+  *          'listening_id'    => Listening ID
+  *          'user_id'         => User ID
+  *
+  * @return int Insert ID or boolean FALSE.
+  */
+if (!function_exists('addListeningFormatTypes')) {
+  function addListeningFormatTypes($opts = array()) {
+    $ci=& get_instance();
+    $ci->load->database();
+
+    $format_type_id = !empty($opts['format_type_id']) ? $opts['format_type_id'] : '';
+    $listening_id = $opts['listening_id'];
+    $user_id = $opts['user_id'];
+    $sql = "INSERT
+              INTO " . TBL_listening_format_types . " (`listening_id`, `listening_format_type_id`, `user_id`)
+              VALUES (" . $ci->db->escape($listening_id) . ", " . $ci->db->escape($format_type_id) . ", " . $ci->db->escape($user_id) . ")";
+    $query = $ci->db->query($sql);
+    if($ci->db->affected_rows() == 1) {
+      return $ci->db->insert_id();
+    }
+    else {
+      return FALSE;
+    }
+  }
+}
+?>
