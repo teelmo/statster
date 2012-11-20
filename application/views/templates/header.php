28c62ae6a822536e8bb5a7a25ebd759b2529fc8d
Teemo Tebest <teemo.tebest@gmail.com>
2012-11-20 12:24:11 +0200

    Added viewport meta definition to reset the default functionality for scaling in most mobile browsers.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
   <head>
     <title><?=TITLE?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="/media/js/jquery-1.8.2.min.js"></script>
     <script type="text/javascript" src="/media/js/jquery.autocomplete.min.js"></script>
     <script type="text/javascript" src="/media/js/dropdown.js"></script>
