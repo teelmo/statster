23d9dcc157e27de3ce9dfd821feb0265d214f98f
Teemo Tebest <teemo.tebest@gmail.com>
2013-06-16 18:26:24 +0300

    Updated session update time. Now session doesn't expire.

--- application/config/config.php
+++ application/config/config.php
@@ -246,7 +246,7 @@ $config['encryption_key'] = '7149c57ae837dc36b910fe79a5558ddd';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
