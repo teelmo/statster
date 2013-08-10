0d4f0f6e5812ce15d475fd4581abb447a8b52b4e
Teemo Tebest <teemo.tebest@gmail.com>
2013-08-10 21:58:14 +0300

    Added an alternative background color for artists and albums big bar tables.

--- application/views/music/albums_view.php
+++ application/views/music/albums_view.php
@@ -4,7 +4,7 @@
     <img src="/media/img/ajax-loader-bar.gif" alt="" class="loader" id="topAlbum10Loader"/>
     <ul id="topAlbum10" class="chartList chartList124 noBullets"><!-- Content is loaded with AJAX --></ul>
     <img src="/media/img/ajax-loader-bar.gif" alt="" class="loader" id="topAlbumLoader"/>
-    <table id="topAlbum" class="barTable"><!-- Content is loaded with AJAX --></table>
+    <table id="topAlbum" class="barTable bigBarTable"><!-- Content is loaded with AJAX --></table>
   </div>
   <div class="container"><hr /></div>
 </div>
--- application/views/music/artists_view.php
+++ application/views/music/artists_view.php
@@ -4,7 +4,7 @@
     <img src="/media/img/ajax-loader-bar.gif" alt="" class="loader" id="topArtist10Loader"/>
     <ul id="topArtist10" class="chartList chartList124 noBullets"><!-- Content is loaded with AJAX --></ul>
     <img src="/media/img/ajax-loader-bar.gif" alt="" class="loader" id="topArtistLoader"/>
-    <table id="topArtist" class="barTable"><!-- Content is loaded with AJAX --></table>
+    <table id="topArtist" class="barTable bigBarTable"><!-- Content is loaded with AJAX --></table>
   </div>
   <div class="container"><hr /></div>
 </div>
