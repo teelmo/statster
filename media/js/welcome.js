cbbcab56948e88f5977d118a1da95451bcbedded
Teemo Tebest <teemo.tebest@gmail.com>
2013-11-26 22:43:25 +0200

    Rewritten welcome javascript file in more object oriented way.

--- media/js/welcome.js
+++ media/js/welcome.js
@@ -1,64 +1,54 @@
-$(document).ready(function() {
-  recentlyListened();
-  topArtist();
-});
-
-function recentlyListened() {
-  $.ajax({
-    type:'GET',
-    dataType:'json',
-    url:'/api/listening/get',
-    data: {
-      limit:5,
-    },
-    statusCode: {
-      200: function(data) {
-        $.ajax({
-          type:'POST',
-          url:'/ajax/sideTable',
-          data: {
-            json_data:data,
-            size:32,
-            hide : {
-              artist:true,
-              count:true,
-              rank:true,
-              calendar:true
+var view = {
+  recentlyListened: function () {
+    $.ajax({
+      type:'GET',dataType:'json',url:'/api/listening/get',
+      data: {
+        limit:5,
+      },
+      statusCode: {
+        200: function(data) {
+          $.ajax({
+            type:'POST',url:'/ajax/sideTable',
+            data: {
+              json_data:data,size:32,
+              hide : {
+                artist:true,count:true,rank:true,calendar:true
+              },
             },
-          },
-          success: function(data) {
-            $('#recentlyListenedLoader').hide();
-            $('#recentlyListened').html(data);
-          }
-        });
+            success: function(data) {
+              $('#recentlyListenedLoader').hide();
+              $('#recentlyListened').html(data);
+            }
+          });
+        }
       }
-    }
-  });
+    });
+  },
+  topArtist: function () {
+    $.ajax({
+      type:'GET',dataType:'json',url:'/api/artist/get',
+      data: {
+        lower_limit:'1970-01-01',limit:15
+      },
+      statusCode: {
+        200: function(data) {
+          $.ajax({
+            type:'POST',url:'/ajax/barTable',
+            data: {
+              json_data:data,
+            },
+            success: function(data) {
+              $('#topArtistLoader').hide();
+              $('#topArtist').html(data);
+            }
+          });
+        }
+      }
+    });
+  }
 }
 
-function topArtist() {
-  $.ajax({
-    type:'GET',
-    dataType:'json',
-    url:'/api/artist/get',
-    data: {
-      lower_limit:'1970-01-01',
-      limit:15,
-    },
-    statusCode: {
-      200: function(data) {
-        $.ajax({
-          type:'POST',
-          url:'/ajax/barTable',
-          data: {
-            json_data:data,
-          },
-          success: function(data) {
-            $('#topArtistLoader').hide();
-            $('#topArtist').html(data);
-          }
-        });
-      }
-    }
-  });
-}
+$(document).ready(function() {
+  view.recentlyListened();
+  view.topArtist();
+});
