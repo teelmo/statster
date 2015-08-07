bdd857ceb01ee71628f0fbfef9527de34cf9202c
Teemo Tebest <teemo.tebest@gmail.com>
2015-08-07 14:07:16 +0300

    Updated CSS file.

--- media/js/profile.js
+++ media/js/profile.js
@@ -19,8 +19,7 @@ $.extend(view, {
             data:{
               json_data:data,
               hide:{
-                del:true,
-                user:true
+                del:true
               }
             },
             success: function (data) {
