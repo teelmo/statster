7c30612f17a76480f624d2d539c05f6a62c5561e
Teemo Tebest <teemo.tebest@gmail.com>
2015-11-09 12:59:34 +0200

    Fixed a bug in music view when calculating listener count.

--- application/controllers/music.php
+++ application/controllers/music.php
@@ -20,10 +20,12 @@ class Music extends CI_Controller {
     unset($data['user_id']);
     $data['logged_in'] = ($this->session->userdata('logged_in') === TRUE) ? TRUE : FALSE;
     $data += getAlbumListenings($data);
+    // pr($data);
     if ($data['user_id'] = $this->session->userdata('user_id')) {
       $data += getAlbumListenings($data);
     }
     $data += getAlbumTags($data);
+    unset($data['username']);
     $data['listener_count'] = sizeof(json_decode(getListeners($data), true));
     $data['spotify_id'] = getSpotifyResourceId($data['artist_name'], $data['album_name']);
 
