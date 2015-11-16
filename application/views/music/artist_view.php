f7d7f27363e6b2cd8b8486d1d1574d48947e17ae
Teemo Tebest <teemo.tebest@gmail.com>
2015-11-16 09:39:19 +0200

    Added number_format to user's listenings on artist and album view.

--- application/views/music/artist_view.php
+++ application/views/music/artist_view.php
@@ -44,7 +44,7 @@
           <?php
           if ($logged_in === TRUE) {
             ?>
-            <td class="label user_listening" rowspan="3"><div class="user_listenings_img cover img32" style="background-image: url('<?=getUserImg(array('user_id' => $this->session->userdata('user_id'), 'size' => 32))?>');"></div><span class="user_value"><span class="value"><?=anchor(array('recent', url_title($artist_name) . '?u=' . $this->session->userdata('username')), $user_count)?></span> in your library</span></td>
+            <td class="label user_listening" rowspan="3"><div class="user_listenings_img cover img32" style="background-image: url('<?=getUserImg(array('user_id' => $this->session->userdata('user_id'), 'size' => 32))?>');"></div><span class="user_value"><span class="value"><?=anchor(array('recent', url_title($artist_name) . '?u=' . $this->session->userdata('username')), number_format($user_count))?></span> in your library</span></td>
             <?php
           }
           ?>
