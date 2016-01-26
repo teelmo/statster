4dbb95566586b56264cc472ab950efd5cc24394f
Teemo Tebest <teemo.tebest@gmail.com>
2016-01-26 13:34:32 +0200

    Removed debug output.

--- application/views/site_templates/header.php
+++ application/views/site_templates/header.php
@@ -41,7 +41,7 @@
             <div class="user_container">
               <div class="profile_text"><?=$this->session->userdata('username')?></div>
               <div class="profile_img" style="background-image: url('<?=$this->session->userdata('user_image')?>');"><img src="<?=$this->session->userdata('user_image')?>" alt="" /></div>
-              <ul class="subnav" style="display: none;">${!${false}=$this->session->userdata('get_username')}
+              <ul class="subnav" style="display: none;">
                 <li><a href="javascript:;" class="toggle_username <?=(!empty(${!${false}=$this->session->userdata('get_username')})) ? 'active' : ''; ?>"><?=(!empty(${!${false}=$this->session->userdata('get_username')})) ? 'Show all' : 'Your stats only'; ?></a></li>
                 <li><?=anchor(array('user', $this->session->userdata('username')), 'Profile')?></li>
                 <li><?=anchor(array('user', 'edit'), 'Edit')?></li>
