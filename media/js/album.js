d839019f9100374940990cd66f8b00033060bd1c
Teemo Tebest <teemo.tebest@gmail.com>
2017-07-24 17:24:34 +0300

    Fixed listening graphs in artist and album views.

--- media/js/album.js
+++ media/js/album.js
@@ -99,7 +99,7 @@ $.extend(view, {
   },
   getListeningHistory: function (type) {
     if (type == '%w') {
-      var where = 'DATE_FORMAT(<?=TBL_listening?>.`date`, \'' + type + '\') IS NOT NULL';
+      var where = 'DATE_FORMAT(<?=TBL_listening?>.`date`, \'' + type + '\') IS NOT NULL AND DATE_FORMAT(<?=TBL_listening?>.`date`, \'%d\') != \'00\'';
     }
     else if (type == '%Y%m') {
       var where = 'DATE_FORMAT(<?=TBL_listening?>.`date`, \'%m\') != \'00\'';
