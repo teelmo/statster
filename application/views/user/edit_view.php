085e3e63a3d337d141fa4b2fa52a460731c8cb03
Teemo Tebest <teemo.tebest@gmail.com>
2017-08-05 13:38:17 +0300

    More extensions.

--- application/views/user/edit_view.php
+++ application/views/user/edit_view.php
@@ -58,7 +58,7 @@
             foreach ($formats as $format_key => $format) {
               if (isset($format->format_types)) {
                 ?>
-                <label><div class="middle checkbox_container"><input type="checkbox" name="listening_formats[]" value="<?=$format->format_name?>" <?php if (in_array($format->format_name, $listening_formats)) { echo 'checked="checked"'; }?> /> <img src="/media/img/format_img/format_icons/<?=$format->format_img?>" alt="" class="middle"/> <?=ucfirst($format->format_name)?></div></label>
+                <label><div class="middle checkbox_container"><input type="checkbox" name="listening_formats[]" value="<?=$format->format_name?>" <?php if (in_array($format->format_name, $listening_formats)) { echo 'checked="checked"'; }?> /> <img src="/media/img/format_img/format_icons/<?=$format->format_img?>.png" alt="" class="middle"/> <?=ucfirst($format->format_name)?></div></label>
                 <?php
                 foreach ($format->format_types as $format_type_key => $format_type) {
                   ?>
