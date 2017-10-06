04b25587e094e8633a6b3c3d619ec8884407e842
Teemo Tebest <teemo.tebest@gmail.com>
2017-10-06 10:42:03 +0300

    Started to implement artist and album yearly, monthly and daily stats.

--- application/controllers/Album.php
+++ application/controllers/Album.php
@@ -10,13 +10,39 @@ class Album extends CI_Controller {
     $this->load->helper(array('music_helper', 'img_helper', 'output_helper'));
 
     $data = array();
-    $data['lower_limit'] = '1970-01-01';
+    $data['lower_limit'] = '1970-00-00';
     $data['upper_limit'] = CUR_DATE;
     $data['title'] = 'Albums';
-    $data['year'] = '';
-    $data['month'] = '';
-    $data['day'] = '';
     $data['side_title'] = 'Yearly';
+    $data['day'] = '';
+    $data['month'] = '';
+    $data['year'] = '';
+
+    $opts = array(
+      'human_readable' => false,
+      'limit' => '1',
+      'lower_limit' => date('Y-m', strtotime('first day of last month')) . '-00',
+      'upper_limit' => date('Y-m', strtotime('first day of last month')) . '-31'
+    );
+    $data['top_artist'] = (json_decode(getArtists($opts), true) !== NULL) ? json_decode(getArtists($opts), true)[0] : array();
+    $data['js_include'] = array('albums');
+      
+    $this->load->view('site_templates/header');
+    $this->load->view('music/albums_view', $data);
+    $this->load->view('site_templates/footer');
+  }
+  public function stats($year, $month = FALSE) {
+    // Load helpers.
+    $this->load->helper(array('music_helper', 'img_helper', 'output_helper'));
+    
+    $data = array();
+    $data['lower_limit'] = $year . '-' . (($month === FALSE) ? '00' : $month) . '-00';
+    $data['upper_limit'] = $year . '-' . (($month === FALSE) ? '12' : $month) . '-31';
+    $data['title'] = (($month === FALSE) ? 'Albums '. $year : 'Albums '. $year . ' ' . DateTime::createFromFormat('!m', $month)->format('F'));
+    $data['side_title'] = 'Yearly';
+    $data['day'] = '';
+    $data['month'] = $month;
+    $data['year'] = $year;
 
     $opts = array(
       'human_readable' => false,
--- application/controllers/Artist.php
+++ application/controllers/Artist.php
@@ -10,13 +10,39 @@ class Artist extends CI_Controller {
     $this->load->helper(array('music_helper', 'img_helper', 'output_helper'));
     
     $data = array();
-    $data['lower_limit'] = '1970-01-01';
+    $data['lower_limit'] = '1970-00-00';
     $data['upper_limit'] = CUR_DATE;
     $data['title'] = 'Artists';
-    $data['year'] = '';
-    $data['month'] = '';
-    $data['day'] = '';
     $data['side_title'] = 'Yearly';
+    $data['day'] = '';
+    $data['month'] = '';
+    $data['year'] = '';
+
+    $opts = array(
+      'human_readable' => false,
+      'limit' => '1',
+      'lower_limit' => date('Y-m', strtotime('first day of last month')) . '-00',
+      'upper_limit' => date('Y-m', strtotime('first day of last month')) . '-31'
+    );
+    $data['top_artist'] = (json_decode(getArtists($opts), true) !== NULL) ? json_decode(getArtists($opts), true)[0] : array();
+    $data['js_include'] = array('artists');
+    
+    $this->load->view('site_templates/header');
+    $this->load->view('music/artists_view', $data);
+    $this->load->view('site_templates/footer');
+  }
+  public function stats($year, $month = FALSE) {
+    // Load helpers.
+    $this->load->helper(array('music_helper', 'img_helper', 'output_helper'));
+    
+    $data = array();
+    $data['lower_limit'] = $year . '-' . (($month === FALSE) ? '00' : $month) . '-00';
+    $data['upper_limit'] = $year . '-' . (($month === FALSE) ? '12' : $month) . '-31';
+    $data['title'] = (($month === FALSE) ? 'Artists '. $year : 'Artists '. $year . ' ' . DateTime::createFromFormat('!m', $month)->format('F'));
+    $data['side_title'] = ($month === FALSE) ? 'Monthly': 'Daily';
+    $data['day'] = '';
+    $data['month'] = $month;
+    $data['year'] = $year;
 
     $opts = array(
       'human_readable' => false,
