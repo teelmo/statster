7fef4b9ef4a25cfb253ab81de50f7985b03a5503
Teemo Tebest <teemo.tebest@gmail.com>
2017-10-20 12:16:15 +0300

    Changed artist and album lists.

--- application/views/templates/album_list.php
+++ application/views/templates/album_list.php
@@ -4,9 +4,9 @@ if (!empty($json_data)) {
     foreach ($json_data as $idx => $row) {
       ?>
       <li>
-        Released: <span class="album_year number"><?=anchor(array('year', $row['year']), $row['year'], array('title' => 'Browse release year'))?></span>
-        <?=anchor(array('music', url_title($row['artist_name']), url_title($row['album_name'])), '<div class="cover album_img img150" style="background-image:url(' . getAlbumImg(array('album_id' => $row['album_id'], 'size' => 174)) . ')">' . anchor(array('music', url_title($row['artist_name']), url_title($row['album_name'])), '<span class="title">' . substrwords($row['album_name'], 35) . '</span>', array('title' => 'Browse to album\'s page')) . '</div>', array('title' => 'Browse to album\'s page'))?>
-        <span class="play_count number"><?=$row['count']?></span> plays
+        <div class="metainfo">Released: <span class="album_year number"><?=anchor(array('year', $row['year']), $row['year'], array('title' => 'Browse release year'))?></span></div>
+        <?=anchor(array('music', url_title($row['artist_name']), url_title($row['album_name'])), '<div class="cover album_img img150" style="background-image:url(' . getAlbumImg(array('album_id' => $row['album_id'], 'size' => 174)) . ')"><div class="meta"><div class="title main">' . anchor(array('music', url_title($row['artist_name']), url_title($row['album_name'])), substrwords($row['album_name'], 35), array('title' => 'Browse to album\'s page')) . '</div><div class="title">' . anchor(array('music', url_title($row['artist_name'])), '' . substrwords($row['artist_name'], 35), array('title' => 'Browse to artist\'s page')) . '</div></div></div>', array('title' => 'Browse to album\'s page'))?>
+        <div class="play_count number"><?=$row['count']?></div> plays
       </li>
       <?php
     }
