5a8ea5dbbb721d175f18e678fc8e8ee81fcc92de
Teemo Tebest <teemo.tebest@gmail.com>
2018-06-13 16:12:52 +0300

    Added listening remove functionality to album's recent view

--- media/js/recent_album.js
+++ media/js/recent_album.js
@@ -259,6 +259,36 @@ $.extend(view, {
       });
       $('#tagAdd').hide();
     });
+    $('html body').on('click', 'span.delete', function () {
+      $($(this).data('confirmation-container')).show();
+    });
+    $('html body').on('click', 'a.cancel', function () {
+      $(this).closest('div').hide();
+    });
+    $('html body').on('click', 'a.confirm', function () {
+      var row_id = $(this).data('row-id');
+      if ($('#' + row_id).hasClass('just_added')) {
+        $('tr').removeClass('just_added_rest');
+      }
+      $.ajax({
+        statusCode:{
+          200: function () { // 200 OK
+            $('#' + row_id).fadeOut('slow');
+          },
+          400: function () { // 400 Bad Request
+            alert('400 Bad Request');
+          },
+          401: function (data) { // 403 Forbidden
+            alert('401 Unauthorized');
+          },
+          404: function () { // 404 Not found
+            alert('404 Not Found');
+          }
+        },
+        type:'DELETE',
+        url:'/api/listening/delete/' + $(this).data('listening-id')
+      });
+    });
   }
 });
 
