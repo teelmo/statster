3ed4c9dd43663960b6e3fee279fd94a0e5a41d73
Teemo Tebest <teemo.tebest@gmail.com>
2018-07-11 09:33:06 +0300

    Updated CI from 3.1.8 to 3.1.9.

--- system/database/drivers/mysqli/mysqli_driver.php
+++ system/database/drivers/mysqli/mysqli_driver.php
@@ -387,9 +387,9 @@ class CI_DB_mysqli_driver extends CI_DB {
 	 * @return	string
 	 */
 	protected function _escape_str($str)
-	{
-		return preg_replace('/\\\\\'([0-9{4}\-0-9{2}\-0-9{2}]{10}|[00]*[Ymdw\-%]*|[0-9]{4}%|[0-9{4}\-0-9{2}]{7}%)\\\\\'/', '\'${1}\'', $this->conn_id->real_escape_string($str));
-	}
+  {
+    return preg_replace('/\\\\\'([0-9{4}\-0-9{2}\-0-9{2}]{10}|[00]*[Ymdw\-%]*|[0-9]{4}%|[0-9{4}\-0-9{2}]{7}%)\\\\\'/', '\'${1}\'', $this->conn_id->real_escape_string($str));
+  }
 
 	// --------------------------------------------------------------------
 
