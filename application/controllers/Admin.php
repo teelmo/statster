74db3b1043075830f93197543a6c5d961857c912
Teemo Tebest <teemo.tebest@gmail.com>
2019-07-18 11:02:09 +0200

    Implemented admin view a little further.

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,9 +9,17 @@ class Admin extends CI_Controller {
     if (in_array($this->session->userdata['user_id'], ADMIN_USERS)) {
 
       // Load helpers.
-      $this->load->helper(array());
+      $this->load->helper(array('form', 'img_helper', 'music_helper', 'output_helper'));
       
       $data = array();
+
+      $opts = array(
+        'human_readable' => false,
+        'limit' => '1',
+        'lower_limit' => date('Y-m', strtotime('first day of last month')) . '-00',
+        'upper_limit' => date('Y-m', strtotime('first day of last month')) . '-31'
+      );
+      $data['top_artist'] = (json_decode(getArtists($opts), true) !== NULL) ? json_decode(getArtists($opts), true)[0] : array('name' => 'No data', 'count' => 0);
       
       $this->load->view('site_templates/header');
       $this->load->view('admin/admin_view', $data);
--- application/views/admin/admin_view.php
+++ application/views/admin/admin_view.php
@@ -1,7 +1,49 @@
-<div class="main_container no_heading">
+<div class="heading_container">
+  <div class="heading_cont main_heading_cont" style="background-image: url('<?=getArtistImg(array('artist_id' => $top_artist['artist_id'], 'size' => 300))?>');">
+    <div class="info">
+      <h1><span class="stats">stats</span><span class="ter">ter</span><span class="separator"></span><span class="meta">reconcile with music</span><div class="top_music"><?=anchor(array('music', date('Y', strtotime('last month')), date('m', strtotime('last month'))), 'Top in ' . date('F', strtotime('last month')))?></div></h1>
+    </div>
+  </div>
+</div>
+<div class="main_container">
+  <div class="page_links">
+    <?=anchor(array('album'), 'Albums')?>
+    <?=anchor(array('artist'), 'Artists')?>
+    <?=anchor(array('format'), 'Formats')?>
+    <?=anchor(array('listener'), 'Listeners')?>
+    <?=anchor(array('like'), 'Likes')?>
+    <?=anchor(array('shout'), 'Shouts')?>
+    <?=anchor(array('tag'), 'Tags')?>
+  </div>
   <div class="left_container">
     <div class="container">
       <h1>Admin</h1>
-      <p>Welcome to the admin section.</p>
+      <p>Unauthorized access not allowed.</p>
+    </div>
+    <div class="container"><hr /></div>
+    <div class="container">
+      <h2>Add genre</h2>
+      <?=form_open('', array('class' => '', 'id' => 'addGenreForm'))?>
+        <div><input type="text" autocomplete="off" tabindex="1" id="addGenreText" placeholder="New genre name" name="addGenreText" /></div>
+        <div><input type="submit" name="addGenreSubmit" tabindex="10" id="addGenreSubmit" value="Add" /></div>
+      </form>
+    </div>
+    <div class="container"><hr /></div>
+    <div class="container">
+      <h2>Add keywords</h2>
+      <?=form_open('', array('class' => '', 'id' => 'addKeywordForm'))?>
+        <div><input type="text" autocomplete="off" tabindex="1" id="addKeywordText" placeholder="New keyword name" name="addKeywordText" /></div>
+        <div><input type="submit" name="addKeywordSubmit" tabindex="10" id="addKeywordSubmit" value="Add" /></div>
+      </form>
+    </div>
+    <div class="container"><hr /></div>
+    <div class="container">
+      <h2>Edit albums</h2>
+      <p>Albums are being edited from the album page</p>
+    </div>
+    <div class="container"><hr /></div>
+    <div class="container">
+      <h2>Edit artists</h2>
+      <p>Artists are being edited from the album page</p>
     </div>
   </div>
