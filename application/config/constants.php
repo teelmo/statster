f0350dd93e65734865c88a3e1f3ee6a0f44472cb
Teemo Tebest <teemo.tebest@gmail.com>
2021-05-31 11:19:13 +0200

    Fixed a bug in constant values

--- application/config/constants.php
+++ application/config/constants.php
@@ -138,7 +138,7 @@ define('INTERVAL_TEXTS', array(
 define('ORDER_TEXTS', array(
   '`count` DESC' => 'Count',
   '`albums`.`album_name` ASC' => 'Name',
-  '`albums`.`year` DESC' => 'Year'
+  '`albums`.`year` DESC, `count` DESC' => 'Year'
 ));
 
 /* API keys */
