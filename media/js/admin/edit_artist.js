324c16dd8dbc21e2bae3b7e6d6e803320e31a35e
Teemo Tebest <teemo.tebest@gmail.com>
2021-07-13 21:01:58 +0200

    Fixed a bug in admit edit artist

--- media/js/admin/edit_artist.js
+++ media/js/admin/edit_artist.js
@@ -2,10 +2,10 @@ $.extend(view, {
   // Init Edit album events.
   initEditArtistEvents: function () {
     $('.spotify_id').blur(function() {
-      $(this).val(($(this).val().replace(/(https:\/\/open\.spotify\.com\/album\/)?([a-zA-Z0-9]{22})(.*)/, '$2')));
+      $(this).val(($(this).val().replace(/(https:\/\/open\.spotify\.com\/artist\/)?([a-zA-Z0-9]{22})(.*)/, '$2')));
     });
     $('.spotify_id').keyup(function() {
-      $(this).val(($(this).val().replace(/(https:\/\/open\.spotify\.com\/album\/)?([a-zA-Z0-9]{22})(.*)/, '$2')));
+      $(this).val(($(this).val().replace(/(https:\/\/open\.spotify\.com\/artist\/)?([a-zA-Z0-9]{22})(.*)/, '$2')));
     });
   }
 });
