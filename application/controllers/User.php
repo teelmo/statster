64372e9bc9d9d6c41e5325424e98dd2cd9472af9
Teemo Tebest <teemo.tebest@gmail.com>
2022-01-21 13:37:36 +0100

    Only including add listening helper if user is logged in.

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -66,7 +66,10 @@ class User extends CI_Controller {
         $data['similarity'] = getUserSimilarity($data);
       }
       $data['logged_in'] = ($this->session->userdata('logged_in') === TRUE) ? 'true' : 'false';
-      $data['js_include'] = array('user/profile', 'libs/highcharts', 'libs/peity', 'helpers/chart_helper', 'helpers/comment_helper', 'helpers/add_listening_helper', 'helpers/time_interval_helper');
+      $data['js_include'] = array('user/profile', 'libs/highcharts', 'libs/peity', 'libs/jquery.daterangepicker', 'helpers/chart_helper', 'helpers/comment_helper', 'helpers/time_interval_helper');
+      if ($data['logged_in'] === 'true') {
+        $data['js_include'][] = 'helpers/add_listening_helper';
+      }
       
       $this->load->view('site_templates/header');
       $this->load->view('user/profile_view', $data);
