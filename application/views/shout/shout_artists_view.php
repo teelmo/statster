6dd1a5caa8f0014ab65ad65c9189f143187f0d1b
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-12 01:00:28 +0200

    Made statster in heading a link to front page

--- application/views/shout/shout_artists_view.php
+++ application/views/shout/shout_artists_view.php
@@ -1,7 +1,7 @@
 <div class="heading_container">
   <div class="heading_cont main_heading_cont" style="background-image: url('<?=getArtistImg(array('artist_id' => $top_artist['artist_id'], 'size' => 300))?>');">
     <div class="info">
-      <h1><span class="stats">stats</span><span class="ter">ter</span><span class="separator"></span><span class="meta">reconcile with music</span><div class="top_music"><?=anchor(array('music', date('Y', strtotime('last month')), date('m', strtotime('last month'))), 'Top in ' . date('F', strtotime('last month')))?></div></h1>
+      <h1><a href="/" class="statster"><span class="stats">stats</span><span class="ter">ter</span></a><span class="separator"></span><span class="meta">reconcile with music</span><div class="top_music"><?=anchor(array('music', date('Y', strtotime('last month')), date('m', strtotime('last month'))), 'Top in ' . date('F', strtotime('last month')))?></div></h1>
     </div>
   </div>
  <?php
