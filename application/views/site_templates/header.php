46f23233c5a9cd9891039ab97e1ff45b6a3c874e
Teemo Tebest <teemo.tebest@gmail.com>
2023-07-05 15:06:46 +0200

    Updated jQuery

--- application/views/site_templates/header.php
+++ application/views/site_templates/header.php
@@ -23,7 +23,7 @@ header('HTTP/1.1 200 OK');
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script type="text/javascript" src="/media/js/libs/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="/media/js/libs/jquery-1.12.2.min.js"></script>
     <script type="text/javascript" src="/media/js/libs/jquery-ui-1.10.3.custom.min.js"></script>
     <script type="text/javascript" src="/media/js/libs/chosen.jquery.min.js"></script>
     <script type="text/javascript" src="/media/js/libs/moment.min.js"></script>
