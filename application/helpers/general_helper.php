463caaecb54c32e028118ba998fd2207623ac160
Teemo Tebest <teemo.tebest@gmail.com>
2024-02-07 10:17:56 +0100

    PHP7 compability.

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -7,4 +7,13 @@ $ci->load->library('session');
 if (!empty($_SESSION['get_username']) && empty($_GET['u'])) {
   $_GET['u'] = $_SESSION['get_username'];
 }
+
+/* Based on original work from the PHP Laravel framework
+ * source: https://www.php.net/manual/en/function.str-contains.php
+ */
+if (!function_exists('str_contains')) {
+  function str_contains($haystack, $needle) {
+    return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+  }
+}
 ?>
