62f7edbad49f1d4367cb2114fbc9221a0fc83f28
Teemo Tebest <teemo.tebest@gmail.com>
2025-04-21 15:16:13 +0200

    Updated artist/album list

--- application/views/templates/artist_list.php
+++ application/views/templates/artist_list.php
@@ -3,16 +3,16 @@ if (!empty($json_data)) {
   if (empty($json_data['error'])) {
     foreach ($json_data as $idx => $row) {
       ?>
-      <li>
+      <li class="artist">
         <?php
         if (getArtistID($row)) {
           ?>
           <?=(isset($row['country'])) ? anchor(array('nationality', url_title($row['country'])), '<div>' . $row['country'] . ' <img src="/media/img/flag_img/' . strtolower($row['country_code']) . '.png"/ alt="' . $row['name'] . '" /></div>') : ''?>
-          <?=anchor(array('music', url_title($row['artist_name'])), '<div class="cover artist_img img150" style="background-image:url(' . getArtistImg(array('artist_id' => $row['artist_id'], 'size' => 174)) . ')"><div class="meta"><div class="title main">' . anchor(array('music', url_title($row['artist_name'])), '' . substrwords($row['artist_name'], 35), array('title' => 'Browse to artist\'s page')) . '</div></div></div>', array('title' => 'Browse to artist\'s page'))?>
+          <?=anchor(array('music', url_title($row['artist_name'])), '<div class="cover artist_img img150" style="background-image:url(' . getArtistImg(array('artist_id' => $row['artist_id'], 'size' => 174)) . ')"></div><div class="meta"><div class="title main">' . anchor(array('music', url_title($row['artist_name'])), substrwords($row['artist_name'], 35), array('title' => 'Browse to artist\'s page')) . '</div></div>', array('title' => 'Browse to artis\'s page'))?>
           <?php
         }
         else {
-          echo '<div class="cover artist_img img150" style="background-image:url(' . getArtistImg(array('artist_id' => $row['artist_id'], 'size' => 174)) . ')"><div class="meta"><div class="title main">' . $row['artist_name'] . '</div></div></div>';
+          echo '<div class="cover artist_img img150" style="background-image:url(' . getArtistImg(array('artist_id' => $row['artist_id'], 'size' => 174)) . ')"></div><div class="meta"><div class="title main">' . substrwords($row['artist_name'], 35) . '</div></div>';
         }
         if (empty($hide['count'])) {
           ?>
