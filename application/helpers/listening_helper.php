cb89cdf5cfe03861a43d703f0ee7448f358b181e
Teemo Tebest <teemo.tebest@gmail.com>
2025-05-01 15:11:27 +0200

    Changed caching file naming

--- application/helpers/listening_helper.php
+++ application/helpers/listening_helper.php
@@ -25,10 +25,10 @@ if (!function_exists('getListenings')) {
     $artist_id = (isset($opts['artist_name']) && !isset($opts['album_name'])) ? getArtistID($opts) : '%';
 
     $cache_key = ($artist_id !== '%') 
-      ? 'get_listenings-artist_' . md5($artist_id) . '_' . md5(json_encode($opts)) 
+      ? 'c_get_listenings-artist_' . md5($artist_id) . '_' . md5(json_encode($opts)) 
       : (($album_id !== '%') 
-        ? 'get_listenings-album_' . md5($album_id) . '_' . md5(json_encode($opts)) 
-        : 'get_listenings_' . md5(json_encode($opts)));
+        ? 'c_get_listenings-album_' . md5($album_id) . '_' . md5(json_encode($opts)) 
+        : 'c_get_listenings_' . md5(json_encode($opts)));
     if ($cached = $ci->cache->file->get($cache_key)) {
       return $cached;
     }
@@ -155,9 +155,9 @@ if (!function_exists('addListening')) {
       if ($ci->db->affected_rows() === 1) {
         $data['listening_id'] = $ci->db->insert_id();
 
-        clear_cache_by_prefix(['get_listenings_', 'get_listenings-album_' . md5($data['album_id'])]);
+        clear_cache_by_prefix(['c_get_listenings_', 'c_get_listenings-album_' . md5($data['album_id'])]);
         foreach (getAlbumArtists($data) as $artist) {
-          clear_cache_by_prefix(['get_listenings-artist_' . md5($artist['id'])]);
+          clear_cache_by_prefix(['c_get_listenings-artist_' . md5($artist['id'])]);
         } 
 
         // Add listening format data to DB.
