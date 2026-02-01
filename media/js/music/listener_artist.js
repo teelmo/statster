d546ff9b3511b25c0a93f2bd015d2fc99981b8f9
Teemo Tebest <teemo.tebest@gmail.com>
2026-02-01 07:28:10 +0100

    Removed ability to remove

--- media/js/music/listener_artist.js
+++ media/js/music/listener_artist.js
@@ -11,7 +11,7 @@ $.extend(view, {
         200: function (data) { // 200 OK
           $.ajax({
             data:{
-              delete:true,
+              delete:false,
               json_data:data,
               logged_in:<?=$logged_in?>
             },
