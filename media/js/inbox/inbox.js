cc681a81bbd25ea0bfaf9ef73ee8c399993eb1c3
Teemo Tebest <teemo.tebest@gmail.com>
2026-05-18 21:48:39 +0200

    Updated inbox level js with Biome.

--- media/js/inbox/inbox.js
+++ media/js/inbox/inbox.js
@@ -1,39 +1,38 @@
 $.extend(view, {
-  getBulletins: function () {
+  getBulletins: () => {
     if (user_id === undefined) {
       $('#bulletinLoader').hide();
       return;
     }
     $.ajax({
-      complete: function () {
+      complete: () => {
         $('#bulletinLoader').hide();
       },
-      data:{
-        user_id:user_id
+      data: {
+        user_id: user_id
       },
-      dataType:'json',
-      statusCode:{
-        200: function () { // 200 OK
+      dataType: 'json',
+      statusCode: {
+        200: () => {
+          // 200 OK
           $('#love').addClass('love_del');
         },
-        204: function () { // 204 No Content
+        204: () => {
+          // 204 No Content
           $('#love').addClass('love_add');
         },
-        400: function () {
-          alert('<?=ERR_BAD_REQUEST?>');
+        400: () => {
+          alert(`<?=ERR_BAD_REQUEST?>`);
         }
       },
-      type:'GET',
-      url:'/api/love/get/<?=$album_id?>'
+      type: 'GET',
+      url: '/api/love/get/<?=$album_id?>'
     });
   },
-  initInboxEvents: function () {
-    
-  }
+  initInboxEvents: () => {}
 });
 
-$(document).ready(function() {
-  document.querySelector('.background_overlay').style.backgroundImage = "url('<?=getArtistImg(array('artist_id' => $top_artist['artist_id'], 'size' => 300))?>')";
+$(document).ready(() => {
+  app.setOverlayBackground(`<?=getArtistImg(array('artist_id' => $top_artist['artist_id'], 'size' => 300))?>`);
   view.initInboxEvents();
 });
-
