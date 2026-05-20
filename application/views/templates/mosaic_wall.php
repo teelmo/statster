adc4730198815237fa16515df61fcecf52a8bacf
Teemo Tebest <teemo.tebest@gmail.com>
2026-05-20 16:40:39 +0200

    Updated user page to include username

--- application/views/templates/mosaic_wall.php
+++ application/views/templates/mosaic_wall.php
@@ -43,7 +43,7 @@ if (!empty($json_data)) {
         foreach ($json_data as $idx => $row) {
           ?>
           <li>
-            <?=anchor(array('user', url_title($row['username'])), '<div class="cover user_img img150" style="background-image:url(' . getUserImg(array('user_id' => $row['user_id'], 'size' => 174)) . ')"></div>', array('title' => 'Browse to user\'s page'))?>
+            <?=anchor(array('user', url_title($row['username'])), '<div class="cover user_img img150" style="background-image:url(' . getUserImg(array('user_id' => $row['user_id'], 'size' => 174)) . ')"><div class="meta"><div class="title main">' . anchor(array('user', url_title($row['username'])), substrwords($row['username'], 35)) . '</div></div></div>', array('title' => 'Browse to user\'s page'))?>
           </li>
           <?php
         }
