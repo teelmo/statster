ae30e7fa8dab4830833b083dc0127083cc74b3b0
Teemo Tebest <teemo.tebest@gmail.com>
2026-09-17 17:42:06 +0200

    Restore sess_expiration to 604800 (7 days) so login sessions survive browser restarts

--- application/config/config.php
+++ application/config/config.php
@@ -369,8 +369,7 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
-// $config['sess_expiration'] = 604800;
-$config['sess_expiration'] = 0;
+$config['sess_expiration'] = 604800;
 $config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
